195da8612f5c5054d370d46c9607b9e63d8bebab
Neto test <2001107@mail.ru>
2024-01-30 20:34:36 +0300

    correction

--- src/main/java/ru/netology/javaqa/Main.java
+++ src/main/java/ru/netology/javaqa/Main.java
@@ -7,7 +7,7 @@ public class Main {
         BonusService service = new BonusService();
 
         int expected = 150;
-        long actual = service.calculate(5_000,true);
+        long actual = service.calculate(5_000, true);
         System.out.println(expected + " == ? == " + actual);
 
     }
